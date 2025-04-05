c411e1a2f31f94cc2d24779cbce8b752d6560b99
Johannes C. Mayer <j.c.mayer240@gmail.com>
2025-04-05 14:19:34 +0200

    fix repl focus switching during eval

--- hy-shell.el
+++ hy-shell.el
@@ -239,7 +239,8 @@ Expected to be called within a Hy interpreter process buffer."
   (declare (indent 0))
   (let ((text-sym (gensym)))
     `(-when-let (,text-sym ,text)
-       (run-hy)
+       (unless (hy-shell--live?)
+         (run-hy))
        (hy-shell--with-live
          ;; TODO Force the initial/end cases in a nicer way if possible
          (hy-shell--send "\n")
